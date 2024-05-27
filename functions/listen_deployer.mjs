bc08ae40a3ab39bc06f9a30976fa08227d4d5baa
nasingham <nasingham.2021@scis.smu.edu.sg>
2024-05-27 17:14:11 +0800

    function

---
+++ functions/listen_deployer.mjs
@@ -0,0 +1,147 @@
+// const {Web3}  = require('web3');
+import Web3 from 'web3';
+
+const web3 = new Web3('https://eth-sepolia.g.alchemy.com/v2/oYTceCr2171uweAFpcDci_A-434gf1Qj');
+const deployer = "0x9eBC30E7506E6Ce36eAc5507FCF0121BaF7AeA57"
+//ABI of Deployer
+const contractABI = [
+    {
+    "inputs": [
+        {
+        "internalType": "address",
+        "name": "_logic",
+        "type": "address"
+        },
+        {
+        "internalType": "bytes",
+        "name": "_data",
+        "type": "bytes"
+        }
+    ],
+    "stateMutability": "payable",
+    "type": "constructor"
+    },
+    {
+    "anonymous": false,
+    "inputs": [
+        {
+        "indexed": false,
+        "internalType": "address",
+        "name": "previousAdmin",
+        "type": "address"
+        },
+        {
+        "indexed": false,
+        "internalType": "address",
+        "name": "newAdmin",
+        "type": "address"
+        }
+    ],
+    "name": "AdminChanged",
+    "type": "event"
+    },
+    {
+    "anonymous": false,
+    "inputs": [
+        {
+        "indexed": true,
+        "internalType": "address",
+        "name": "beacon",
+        "type": "address"
+        }
+    ],
+    "name": "BeaconUpgraded",
+    "type": "event"
+    },
+    {
+    "anonymous": false,
+    "inputs": [
+        {
+        "indexed": true,
+        "internalType": "address",
+        "name": "implementation",
+        "type": "address"
+        }
+    ],
+    "name": "Upgraded",
+    "type": "event"
+    },
+    {
+    "stateMutability": "payable",
+    "type": "fallback"
+    },
+    {
+    "stateMutability": "payable",
+    "type": "receive"
+    },
+    {
+    "anonymous": false,
+    "inputs": [
+        {
+        "indexed": true,
+        "internalType": "address",
+        "name": "deployed",
+        "type": "address"
+        },
+        {
+        "indexed": true,
+        "internalType": "address",
+        "name": "implementation",
+        "type": "address"
+        },
+        {
+        "indexed": true,
+        "internalType": "address",
+        "name": "deployer",
+        "type": "address"
+        },
+        {
+        "indexed": false,
+        "internalType": "address",
+        "name": "titleEscrowFactory",
+        "type": "address"
+        },
+        {
+        "indexed": false,
+        "internalType": "bytes",
+        "name": "params",
+        "type": "bytes"
+        }
+    ],
+    "name": "Deployment",
+    "type": "event"
+    }
+]
+
+
+
+
+export default async (request,context) => {
+
+    try{  
+        await web3.eth.net.isListening();
+        console.log('listening');
+
+    
+        const contract = new web3.eth.Contract(contractABI,deployer);
+
+        function bigintReplacer(key, value) {
+            return typeof value === 'bigint' ? value.toString() : value;
+        }
+    
+    const events = await contract.getPastEvents('Deployment',{
+        fromBlock: 0,
+        toBlock: 'latest',
+    });
+
+
+    const jsonString = JSON.stringify(events, bigintReplacer, 2);
+    return new Response(jsonString, {
+        headers: { 'Content-Type': 'application/json'}
+    })
+        
+    } catch(error){
+        console.log(error);
+        return new Response(JSON.stringify({ error: 'Failed fetching data' }), { headers: { 'Content-Type': 'application/json' } });
+      };
+}   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,44 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+    <p>{{ data }}</p>
+    <p>123</p>
+  </div>
+</template>
+
+
+
+
+<script>
+export default{
+  data() {
+    return{
+      data : null,
+    };
+  },
+  async created(){
+    console.log('created')
+    try{
+      const response = await fetch ('/functions/listen_deployer');
+      if (!response.ok){
+        throw new Error('Network response not ok');
+      }
+      console.log(response);
+      const result = await response.json();
+      this.data=result;
+      console.log(result);
+    
+    } catch (error){
+      console.log(error)
+      this.error = error.toString();
+    }
+  },
+}
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+
+<style>
+
+
+
+</style>
