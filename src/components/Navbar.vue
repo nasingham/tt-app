6d0ebe017ce9bef802a74cbba30c6e17dff4cc8e
nasingham <nasingham.2021@scis.smu.edu.sg>
2024-06-13 16:57:44 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -25,8 +25,8 @@
             <v-list-item :to="{name:'sepolia'}" class="icon" prepend-icon="mdi-alpha-s-circle-outline" title="Sepolia" value="sepolia" ></v-list-item>
             <v-list-item  :to="{name:'gtn'}" class="icon" prepend-icon="mdi-cube" title="Stability GTN" value="stability" ></v-list-item>
             <v-list-item  :to="{name:'stabilityTest'}" class="icon" prepend-icon="mdi-cube-outline" title="Stability Testnet" value="stabilityTest" ></v-list-item>
-            <v-list-item  :to="{name:'xdc'}" class="icon" prepend-icon="mdi-cube-outline" title="XDC Mainnet" value="xdc" ></v-list-item>
-            <v-list-item  :to="{name:'apothem'}" class="icon" prepend-icon="mdi-cube-outline" title="XDC Apothem" value="xdcApothem" ></v-list-item>
+            <v-list-item  :to="{name:'xdc'}" class="icon" prepend-icon="mdi-alpha-x-circle" title="XDC Mainnet" value="xdc" ></v-list-item>
+            <v-list-item  :to="{name:'apothem'}" class="icon" prepend-icon="mdi-alpha-x-circle-outline" title="XDC Apothem" value="xdcApothem" ></v-list-item>
             
           </v-list>
 
