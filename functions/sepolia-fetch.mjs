ad94d8cda66059eca4bf5a247bc7bd76340680f8
nasingham <nasingham.2021@scis.smu.edu.sg>
2024-06-18 17:39:46 +0800

    Update sepolia-fetch.mjs

--- functions/sepolia-fetch.mjs
+++ functions/sepolia-fetch.mjs
@@ -8,48 +8,69 @@ export default async(request,context) => {
     //Use this to force an update to Blob
     
     // const currentTime = 1717064413659 + 61*60*1000;
+    try{
+        const combinedStore = getStore("sepolia");
+        
+        const storedData = await combinedStore.get("data",{type: "json"});
+        const storedTimestamp = await combinedStore.get("timestamp", {type:"json"});
+        console.log(`Stored timestamp: ${storedTimestamp}`);
 
-    const combinedStore = getStore("sepolia");
-    
-    const storedData = await combinedStore.get("data",{type: "json"});
-    const storedTimestamp = await combinedStore.get("timestamp", {type:"json"});
-    console.log(`Stored timestamp: ${storedTimestamp}`);
+        // const CACHE_DURATION = 1*60*1000; //1 hour
 
-    // const CACHE_DURATION = 1*60*1000; //1 hour
+        // const gzipResponse = (data) => {
+        //     return new Promise((resolve, reject) => {
+        //       zlib.gzip(data, (error, result) => {
+        //         if (error) {
+        //           reject(error);
+        //         } else {
+        //           resolve(result);
+        //         }
+        //       });
+        //     });
+        //   };
 
-    // const gzipResponse = (data) => {
-    //     return new Promise((resolve, reject) => {
-    //       zlib.gzip(data, (error, result) => {
-    //         if (error) {
-    //           reject(error);
-    //         } else {
-    //           resolve(result);
-    //         }
-    //       });
-    //     });
-    //   };
+        if (storedData && storedTimestamp){
+            console.log("Taking data from Blob");
+            // console.log(`Cache valid for another: ${((CACHE_DURATION - (currentTime - storedTimestamp)) / 1000 / 60).toFixed(2)} minutes`);
+            const processed = {
+                data : storedData,
+                timestamp : storedTimestamp,
+            };
+            const response = JSON.stringify(processed, null, 2);
 
-    if (storedData && storedTimestamp){
-        console.log("Taking data from Blob");
-        // console.log(`Cache valid for another: ${((CACHE_DURATION - (currentTime - storedTimestamp)) / 1000 / 60).toFixed(2)} minutes`);
-        const processed = {
-            data : storedData,
-            timestamp : storedTimestamp,
-        };
-        const response = JSON.stringify(processed, null, 2);
+            // const compressedResponse = await gzipResponse(response);
 
-        // const compressedResponse = await gzipResponse(response);
-
-        return new Response(response, {
-            headers: { 
-            'Content-Type': 'application/json',
-            // 'Content-Encoding': 'gzip',
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Methods": "GET, POST, OPTION",
-            }
-        }); 
-    }else{
-        console.log('No data found in Blob');
+            return new Response(response, {
+                headers: { 
+                'Content-Type': 'application/json',
+                // 'Content-Encoding': 'gzip',
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "GET, POST, OPTION",
+                }
+            }); 
+        }else{
+            console.log('No data found in Blob')
+            return new Response(JSON.stringify({ error: 'No data found' }), {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Methods': 'GET, POST, OPTION',
+                },
+                status: 404
+            });
+        }
+    }catch (error) {
+        console.error('Error fetching data from Blob:', error);
+        return new Response(JSON.stringify({ error: 'Internal server error' }), {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            },
+            status: 500
+        });
     }
 }
